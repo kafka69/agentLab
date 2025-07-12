69d8b0824602d3ebe9750399c0e2ba1ad3ea8154
kafka69 <lordkev456@gmail.com>
2025-07-13 00:41:09 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ if [ ! -d "venv_agent_lab" ]; then
 fi
 
 # Step 2: Use venv's pip to install requirements (no need to activate)
+pip install --upgrade setuptools wheel
 venv_agent_lab/bin/pip install -r requirements.txt
 
 # Step 3: Run Streamlit globally (outside the venv)
